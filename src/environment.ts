aaeaff6f7e0fbc616e66972b8c152e933d8c36e8
CCleanerShot <31040856+CCleanerShot@users.noreply.github.com>
2024-01-31 23:17:05 -0600

    commiting environment types

--- src/environment.ts
+++ src/environment.ts
@@ -7,6 +7,8 @@ declare global {
 			SUPABASE_CONNECTION_STRING: string;
 			NEXT_PUBLIC_SUPABASE_HOST_URL: string;
 			NEXT_PUBLIC_SUPABASE_PUBLIC_KEY: string;
+			NEXT_PUBLIC_HOMEPAGE_DEV: string;
+			NEXT_PUBLIC_HOMEPAGE_PROD: string;
 		}
 	}
 }
